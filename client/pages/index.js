a81fdbb1ee3515f5766c15fc8f212ce9d4ccb554
Bar1996 <bar3668@gmail.com>
2024-11-11 15:23:32 +0200

    Add signin and signout, handle user if signed in or not

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,18 @@
-const Index = () => {
-  return <h1>Landing Page</h1>;
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
-export default Index;
+LandingPage.getInitialProps = async ({ req }) => {
+  const client = buildClient({ req });
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
