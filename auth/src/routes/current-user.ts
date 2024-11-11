ba8e89c3feb4e636f6970ebad2b55d918e800f86
Bar1996 <bar3668@gmail.com>
2024-11-11 18:06:10 +0200

    Move the errors and the middlewares directories to common file and push it into npm to be global

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@bc96tickets/common';
 
 const router = express.Router();
 
